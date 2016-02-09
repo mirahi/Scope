2d9b61c59a82113808837bc89a6b59656add0ceb
Bang Nguyen <ban@metropolia.fi>
2016-02-09 22:49:17 +0200

    adding Hamburger menu

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name scopeApp
+ * @description
+ * # scopeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('scopeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+
+
