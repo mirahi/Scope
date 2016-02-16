fe960fd73ea833643db70d6da5335d245dde1fa1
mirahi <mira.hiltunen@live.fi>
2016-02-16 11:45:01 +0200

    Layout changes, videos and audio working

--- app/scripts/controllers/mediaController.js
+++ app/scripts/controllers/mediaController.js
@@ -1,3 +1,5 @@
+'use strict';
+
 angular.module('scopeApp')
 	.controller('MediaController', function ($scope, AjaxFactory) {
 		
--- app/scripts/controllers/registerController.js
+++ app/scripts/controllers/registerController.js
@@ -1,3 +1,5 @@
+'use strict';
+
 angular.module('scopeApp')
     .controller('RegisterController', function ($scope, AjaxFactory, MediaService) {
 
