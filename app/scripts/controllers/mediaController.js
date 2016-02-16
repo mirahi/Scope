da01daa3c8da4afdbf868c4f79ec4e2ccd8939bf
Niina Kristiina <niina.rantanen@metropolia.fi>
2016-02-16 18:20:07 +0200

    test

--- app/scripts/controllers/mediaController.js
+++ app/scripts/controllers/mediaController.js
@@ -1,38 +1,40 @@
 angular.module('scopeApp')
 	.controller('MediaController', function ($scope, AjaxFactory) {
-		
-        
-        
-        $scope.getImages = function() {
-            AjaxFactory.getFiles('image')
-                .then(function(success) {
-                    $scope.images = success.data;
-                    console.log(success.data);
-                }, function(error) {
-                    console.log(error.data);
-                });
-        }
 
-        $scope.getAudios = function() {
-            AjaxFactory.getFiles('audio')
-                .then(function(success) {
-                    $scope.audios = success.data;
-                    console.log(success.data);
-                }, function(error) {
-                    console.log(error.data);
-                });
-        }
 
-        $scope.getVideos = function() {
-            AjaxFactory.getFiles('video')
-                .then(function(success) {
-                    $scope.videos = success.data;
-                    console.log(success.data);
-                }, function(error) {
-                    console.log(error.data);
-                });
-        }
-        
-        $scope.getImages();
-        
+
+		$scope.getImages = function () {
+			AjaxFactory.getFiles('image')
+				.then(function (success) {
+					$scope.images = success.data;
+					console.log(success.data);
+				}, function (error) {
+					console.log(error.data);
+				});
+		}
+
+		$scope.getAudios = function () {
+			AjaxFactory.getFiles('audio')
+				.then(function (success) {
+					$scope.audios = success.data;
+					console.log(success.data);
+				}, function (error) {
+					console.log(error.data);
+				});
+		}
+
+		$scope.getVideos = function () {
+			AjaxFactory.getFiles('video')
+				.then(function (success) {
+					$scope.videos = success.data;
+					console.log(success.data);
+				}, function (error) {
+					console.log(error.data);
+				});
+		}
+
+		$scope.getImages();
+		$scope.getAudios();
+		$scope.getVideos();
+
 	});
