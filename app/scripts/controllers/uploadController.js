a346d6cd3377593d3cd6a8d673c4d29fc1f79ddd
Niina Kristiina <niina.rantanen@metropolia.fi>
2016-03-04 20:57:24 +0200

    feedback from upload

--- app/scripts/controllers/uploadController.js
+++ app/scripts/controllers/uploadController.js
@@ -1,26 +1,34 @@
 "use strict";
 
 angular.module('scopeApp')
-    .controller('UploadController', function ($scope, AjaxFactory, $localStorage) {
-        console.log("asdpasd");
-        $scope.setMediaFile = function (element) {
-            $scope.mimeType = element.files[0].type;
-            $scope.type = $scope.mimeType.substr(0, 5);
-             console.log("asdpasd666   " + $scope.type);
-        };
+	.controller('UploadController', function ($scope, AjaxFactory, $localStorage) {
+		console.log("asdpasd");
+		$scope.setMediaFile = function (element) {
+			$scope.mimeType = element.files[0].type;
+			$scope.type = $scope.mimeType.substr(0, 5);
+			console.log("asdpasd666   " + $scope.type);
+		};
 
-        $scope.postFile = function () {
-            console.log($localStorage.userId);
-            var formdata = new FormData($('#upload-form')[0]);
-            formdata.append('user', $localStorage.userId);
-            formdata.append('type', $scope.type);
-            formdata.append('mime-type', $scope.mimeType);
-            var response = AjaxFactory.uploadFile(formdata);
-            response.then(function (success) {
-                console.log(success.data);
-            }, function (err) {
-                console.log(err.data);
-            });
-        };
+		$scope.postFile = function () {
+			console.log($localStorage.userId);
+			var formdata = new FormData($('#upload-form')[0]);
+			formdata.append('user', $localStorage.userId);
+			formdata.append('type', $scope.type);
+			formdata.append('mime-type', $scope.mimeType);
+			var response = AjaxFactory.uploadFile(formdata);
+			response.then(function (success) {
+				angular.element('#upload-form').hide();
+				angular.element('#upload-button').hide();
+				angular.element('#close-button').hide();
+				angular.element('.modal-body').append("<p>File succesfully added</p>");
+				console.log(success.data);
+				window.location.reload();
+			}, function (err) {
+				angular.element('#upload-form').hide();
+				angular.element('#upload-button').hide();
+				angular.element('.modal-body').append("<p>Error: Oops. Something went wront</p>");
+				console.log(err.data);
+			});
+		};
 
-    });
+	});
