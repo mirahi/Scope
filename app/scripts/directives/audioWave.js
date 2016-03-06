1b0ddca33fb6cd184439923d7d77c9e8045cc37d
mirahi <mira.hiltunen@live.fi>
2016-03-06 13:28:15 +0200

    UI improvements, masonry deleted

---
+++ app/scripts/directives/audioWave.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('scopeApp')
+    .directive('ngWavesurfer', function () {
+    return {
+        restrict: 'E',
+
+        link: function ($scope, $element, $attrs) {
+            $element.css('display', 'block');
+
+            var options = angular.extend({ container: $element[0] }, $attrs);
+            var wavesurfer = WaveSurfer.create(options);
+
+            if ($attrs.url) {
+                wavesurfer.load($attrs.url, $attrs.data || null);
+            }
+
+            $scope.$emit('wavesurferInit', wavesurfer);
+        }
+    };
+});
